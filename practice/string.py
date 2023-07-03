17a964c8be3f9aff172a0e15ff3b790fb0e92405
Sarthak Aggarwal <aggarwalsarthak359@gmail.com>
2023-07-03 14:21:01 +0530

    practice

---
+++ practice/string.py
@@ -0,0 +1,8 @@
+name = "sarthak"
+print(name[0:5])
+print(name[0:5:2])
+print(name.capitalize())
+print(name.count("a"))
+print(name.find("a"))
+print(name.replace("a","s"))
+print(name.replace("a","s",1))
--- practice/variable.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-time.sleep(4)
-count = 0
-while count <= 100:
-    pyautogui.typewrite("hlw Good Morning"+str(count))
-    pyautogui.press("enter")
-    count = count+1
